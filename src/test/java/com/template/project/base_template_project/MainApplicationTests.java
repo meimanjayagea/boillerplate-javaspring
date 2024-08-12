30ba2a3e7803aa14b2b7e35f65270110c241bf21
Jaya Meiman <workingsdd@gmail.com>
2024-08-12 15:15:05 +0700

    init boillerplate

---
+++ src/test/java/com/template/project/base_template_project/MainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.template.project.base_template_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
